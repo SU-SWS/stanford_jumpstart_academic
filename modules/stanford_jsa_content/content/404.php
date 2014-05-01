e88447e73a002607467b4156d55a3e6c70db5e02
Shea McKinney <sherakama@gmail.com>
2014-05-01 13:18:00 -0700

    added sub modules

---
+++ modules/stanford_jsa_content/content/404.php
@@ -0,0 +1,6 @@
+<?php global $base_url; ?>
+<p class="summary">Oops! We can&#39;t find that page!</p>
+<p class="summary">We have a new site and things have moved around a bit. Here are some good places to start.</p>
+<h2>Starting points</h2>
+<p><a class="btn" href="<?php echo $base_url; ?>">Homepage</a></p>
+<p><a class="btn" href="<?php echo $base_url; ?>/search">Search this site... </a></p>
